b0a6f1324fd11964f94e66b53ee3259ca9df9eb5
taest <876239615@qq.com>
2023-06-07 20:11:02 +0800

    docs(leetcode): 相交链表

--- src/Main.java
+++ src/Main.java
@@ -277,6 +277,22 @@ public class Main {
 
     }
 
+    /**
+     * 相交链表 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (null == headA || null == headB)  return null;
+        ListNode pa = headA,pb = headB;
+        while (pa!=pb){
+            pa = pa.next == null ? headB : pa.next;
+            pb = pb.next == null ? headA : pb.next;
+        }
+        return pa;
+    }
+
 }
 
 class ListNode {
